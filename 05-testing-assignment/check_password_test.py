77cfb56e16f18343b3ce019c3fad0c72caf30d7b
Lauren Cochran <36395800+LaurenCochran@users.noreply.github.com>
2020-02-21 16:40:35 -0600

    figured it out

--- 05-testing-assignment/check_password_test.py
+++ 05-testing-assignment/check_password_test.py
@@ -1,11 +1,25 @@
 import pytest
 import System
 
-def test_check_password():
+def test_check_password1():
     database = System.System()
     database.load_data()
     username = 'saab'
     password = 'notgoingtoworkhopefully'
+    database.login(username, password)
+
+def test_check_password2():
+    database = System.System()
+    database.load_data()
+    username = 'saab'
+    password = '12121*^^&as'
+    database.login(username, password)
+
+def test_check_password3():
+    database = System.System()
+    database.load_data()
+    username = 'saab'
+    password = 1234
     database.login(username, password)
 
 
