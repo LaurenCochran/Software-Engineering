042d01d40b5e7d9e872ccf37af68dfbbabdb5f0c
Lauren Cochran <36395800+LaurenCochran@users.noreply.github.com>
2020-02-21 16:10:14 -0600

    maybe this worked

--- 05-testing-assignment/check_password_test.py
+++ 05-testing-assignment/check_password_test.py
@@ -2,8 +2,8 @@ import pytest
 import System
 
 def check_password_test(system):
-    username = "saab"
-    password = "notgoingtoworkhopefully"
+    username = 'saab'
+    password = 'notgoingtoworkhopefully'
     system.login(username, password)
 
 @pytest.fixture
