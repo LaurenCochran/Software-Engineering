7d441644686f7067cf9d826cb87e3b48e5a8220f
Lauren Cochran <36395800+LaurenCochran@users.noreply.github.com>
2020-02-23 15:01:16 -0600

    test_change_grades

--- 05-testing-assignment/change_grade_test.py
+++ 05-testing-assignment/change_grade_test.py
@@ -12,4 +12,4 @@ def test_change_grade():
     databaseSystem.reload_data()
     databaseSystem.login('hdjsr7', 'pass1234')
     grades = databaseSystem.usr.check_grades('cloud_computing')
-    assert grades[0] == 50
+    assert grades['assignment1'] == 50
