4ac68f13158f26d7bb9bca4144e1fd0491d5338c
Lauren Cochran <36395800+LaurenCochran@users.noreply.github.com>
2020-02-23 14:40:29 -0600

    test_change_grade

--- 05-testing-assignment/change_grade_test.py
+++ 05-testing-assignment/change_grade_test.py
@@ -7,7 +7,7 @@ import System
 def test_change_grade():
     databaseSystem = System.System()
     databaseSystem.login('cmhbf5', 'bestTA')
-    databseSystem.usr.change_grade('hdjsr7', 'cloud_computing', 'assignment1', 50)
+    databaseSystem.usr.change_grade('hdjsr7', 'cloud_computing', 'assignment1', 50)
     databaseSystem.reload_data()
     databaseSystem.login('hdjsr7', 'pass1234')
     grades = gradeSystem.usr.check_grades('cloud_computing')
